04947227dcfc6a353a9219e79646995a096ecc13
robert <gurgul.robert@gmail.com>
2016-05-25 23:46:46 +0200

    re

--- js/helpers.js
+++ js/helpers.js
@@ -4,14 +4,14 @@ function Helpers() {}
 // it has no relation with any 'Helpers' object instance
 // more info: http://stackoverflow.com/questions/1635116/javascript-class-method-vs-class-prototype-method
 Helpers.createEl = function (tpl, target, method) {
+    var targetContainer = document.querySelector(target);
     var container = document.createElement('div');
     container.innerHTML = tpl;
-    this.el = container.firstChild;
-    var targetContainer = document.querySelector(target);
+    var el = container.firstChild;
     if (method === 'append') {
-        targetContainer.appendChild(this.el);
+        targetContainer.appendChild(el);
     } else {
-        targetContainer.insertBefore(this.el, targetContainer.firstElementChild);
+        targetContainer.insertBefore(el, targetContainer.firstElementChild);
     }
-    return this.el;
+    return el;
 };
